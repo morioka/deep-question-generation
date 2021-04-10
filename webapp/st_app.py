9fac9d1113f934da124908b9404c537eb4c27fbd
Yasuhiro Morioka <ymorioka@gmail.com>
2021-04-10 23:51:36 +0900

    add streamlit app

---
+++ webapp/st_app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from ml_api.ml import QuestionGenerationAPI
+
+# https://qiita.com/irisu-inwl/items/9d49a14c1c67391565f8
+
+def main():
+    st.title('deep-question-generatoin sample')
+
+    '''
+    this is a [t5-base-japanese-question-generation](https://huggingface.co/sonoisa/t5-base-japanese-question-generation) sample.
+    '''
+
+    #@st.cache(allow_output_mutation=True)
+    def load_ml(ml):
+        ml.load()
+
+    #@st.cache( allow_output_mutation=True)
+    def generate(answer_context_list):
+        return ml.generate_questions(answer_context_list)
+
+
+    ml = QuestionGenerationAPI()
+    load_ml(ml)
+
+
+    context_text = title = st.text_area('conxtext', '日本で一番高い山は富士山です。') 
+    answer_text = title = st.text_input('answer', '富士山') 
+
+    generate_button = st.button('Generate question')
+    st.write('generated_question')
+    if generate_button:
+        generated_questions = generate([
+                [answer_text, context_text]
+            ])
+        st.write(generated_questions[0])
+
+if __name__ == '__main__':
+    main()
